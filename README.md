b8f6f380dd0fd0ec3fa18825e4b1771d6bdead5e
Josh Reeves <jr.accounts@joshreeves.net>
2024-10-06 02:23:51 -0500

    Continuing to add documentation.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Practical ARM Assembly
 A collection of source files, binaries and notes created by following Laurie Wired's [Practical ARM Assembly Tutorial](https://www.youtube.com/playlist?list=PLn_It163He32Ujm-l_czgEBhbJjOUgFhg) series.
 
+For more information, please refer to the [documentation](https://josh-reeves.github.io/practical_arm_assembly/).
+
 **NOTE:** All included source code, notes, examples, concepts and other potential intellectual property should be attributed to their orginal authors.
--- docs/scripts/utils.js
+++ docs/scripts/utils.js
@@ -9,3 +9,40 @@ function unhide(id)
     document.getElementById(id).style.display = "block";
 
 }
+
+function toggleStyle (oldClass, newClass, targetID = null)
+{
+    // Seems to work better than toggle, which runs into some weird issues if the if new style is higher up in the stylesheet than the old one:
+    let trueToggle = function(target)
+    {
+        if(target.classList.contains(oldClass))
+            {
+                target.classList.remove(oldClass);
+                target.classList.add(newClass);
+            }
+            else
+            {
+                target.classList.remove(newClass);
+                target.classList.add(oldClass);
+
+            }
+
+    }
+
+    if (targetID!= null)
+    {
+        let target = document.getElementById(targetID);
+
+        for (let child of target.children)
+            trueToggle(child)
+
+        return;
+
+    }
+
+    var elements = document.getElementsByClassName(oldClass);
+
+    for (let element of elements)
+        trueToggle(element);
+
+}
