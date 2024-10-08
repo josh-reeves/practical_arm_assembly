82ec637f2b2bb6b0a202664512977a08b38fd7f7
Josh Reeves <jr.accounts@joshreeves.net>
2024-10-07 21:30:28 -0500

    Adding remaining files from Lesson 4 along with complete notes for Introduction and Lesson 4.

--- source/lesson_04/A32/EOR.s
+++ source/lesson_04/A32/EOR.s
@@ -2,7 +2,9 @@
 
 _start:
 	mov r0, #0x42
-	eor r0, r0, #0x26
+	eor r0, r0, #0x26 //Performs a bitwise Exclusive-OR  operation on the bits of the first and second operands and stores the result in Rd. The second operand can be a register or immediate.
 
 	mov r7, #0x1
 	swi 0
+	
+
--- source/lesson_04/A32/ORR.s
+++ source/lesson_04/A32/ORR.s
@@ -2,7 +2,8 @@
 
 _start:
 	mov r0, #0x42
-	orr r0, r0, #0x26
+	orr r0, r0, #0x26 //Performs a bitwise OR operation on the bits of the first and second operands and stores the result in Rd. The second operand can be a register or an immediate.
 
 	mov r7, #0x1
 	swi 0
+	
