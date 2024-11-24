e017ab0ef87aa54b75f4f3185013a8a0efa6fc50
Josh Reeves <jr.accounts@joshreeves.net>
2024-11-24 17:40:23 -0600

    Renamed some files and folders for consitency.

--- source/lesson_04/a32/MVN.s
+++
@@ -1,9 +0,0 @@
-.global _start
-
-_start:
-	mov r1, #0x42
-	mvn r0, r1 //MVN{s} Rd, (Rm/#Imm): Performs a bitwise Negate operation on the bits of the second operand and stores the result in Rd. The second operand can be either a register or an immediate.
-
-	mov r7, #0x1
-	swi 0
-
--- source/lesson_04/a32/ORR.s
+++
@@ -1,9 +0,0 @@
-.global _start
-
-_start:
-	mov r0, #0x42
-	orr r0, r0, #0x26 //Performs a bitwise OR operation on the bits of the first and second operands and stores the result in Rd. The second operand can be a register or an immediate.
-
-	mov r7, #0x1
-	swi 0
-	
