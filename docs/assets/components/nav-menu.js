c48eb164d087101676c2f45fbf506be9ac0e4243
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2024-12-15 14:53:25 -0600

    Cleaned up a few more things and continued working on documentation.

--- docs/assets/components/nav-menu.js
+++ docs/assets/components/nav-menu.js
@@ -1,67 +1,80 @@
-class NavMenu extends HTMLElement
-{
-    constructor()
+customElements.define(
+    "nav-menu",
+    class extends HTMLElement
     {
-        super();
-
-    }
-
-    connectedCallback()
-    {
-        this.innerHTML =
-        `
-        <div class="menu-container">
-            <div class="menu-item" style="background-color: #27272A;" onclick="window.location.href='./index.html'">
-                <p style="opacity: 20%;">About</p>
-            </div>
-            <div class="menu-item" onclick="toggleStyle('submenu-item', 'submenu-item-active', 'lessons')">
-                <p>Lesson Notes</p>
-            </div>  
-                <div id="lessons">
-                    <div class="submenu-item" onclick="window.location.href='./pages/lessons/introduction/introduction.html'">
-                        <p>Introduction</p>
-                    </div>                        
-                    <div class="submenu-item" onclick="window.location.href='./pages/lessons/lesson-01/lesson-01.html'">
-                        <p>Lesson 01: MOV, Exit Syscall</p>
+        constructor()
+        {
+            super();
+    
+        }
+    
+        connectedCallback()
+        {
+            this.innerHTML =
+                `
+                <div class="menu-container">
+                    <div class="menu-item" onclick="window.location.href='/index.html'">
+                        <p>About</p>
                     </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 02: ADD, SUB, MUL, set CPSR</p>
+                    <div class="menu-item collapsible" onclick="toggleStyle('submenu-item', 'submenu-item-hidden', 'lessons')">
+                        <p>Lesson Notes</p>
+                    </div>  
+                        <div id="lessons">
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/introduction/introduction.html'">
+                                <p>Introduction</p>
+                            </div>                        
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-01/lesson-01.html'">
+                                <p>Lesson 01: MOV, Exit Syscall</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-02/lesson-02.html'">
+                                <p>Lesson 02: ADD, SUB, MUL, set CPSR</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-03/lesson-03.html'">
+                                <p>Lesson 03: Logical Operators</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-04/lesson-04.html'">
+                                <p>Lesson 04: Signed Number Conversion</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-05/lesson-05.html'">
+                                <p>Lesson 05: Shift and Rotate</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-06/lesson-06.html'">
+                                <p>Lesson 06: The CPSR</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-07/lesson-07.html'">
+                                <p>Lesson 07: CMP</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-08/lesson-08.html'">
+                                <p>Lesson 08: Branching</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-09/lesson-09.html'">
+                                <p>Lesson 09: While Loops</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-10/lesson-10.html'">
+                                <p>Lesson 10: Function Calls</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-11/lesson-11.html'">
+                                <p>Lesson 11: LDR and STR</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-12/lesson-12.html'">
+                                <p>Lesson 12: Stack Operations</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-13/lesson-13.html'">
+                                <p>Lesson 13: Push and Pop</p>
+                            </div>
+                            <div class="submenu-item-hidden" onclick="window.location.href='/pages/lessons/lesson-14/lesson-14.html'">
+                                <p>Lesson 14: Hello World!</p>
+                            </div>
+                        </div>
+                    <div class="menu-item">
+                        <p>Resources</p>
                     </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 03: LDR, STR</p>
-                    </div>
-                    <div class="submenu-item" onclick="window.location.href='./pages/lessons/lesson-04/lesson-04.html'">
-                        <p>Lesson 04: Logical Operators</p>
-                    </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 05: Signed Number Conversions</p>
-                    </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 06: Shift and Rotate</p>
-                    </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 07: CMP</p>
-                    </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 08: Branching</p>
-                    </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 09: While Loops</p>
-                    </div>
-                    <div class="submenu-item" >
-                        <p>Lesson 10: Function Calls</p>
-                    </div>
-                    <div class="submenu-item" style="color: white;" >
-                        <p>Lesson 11: Stack Operations</p>
+                    <div class="menu-item">
+                        <p>Miscellaneous</p>
                     </div>
                 </div>
-            <div class="menu-item"><p>Resources</p></div>
-            <div class="menu-item"><p>Miscellaneous</p></div>
-        </div>
-        `;
-
-    }
-
-}
-
-customElements.define("nav-menu", NavMenu);
+                `;
+    
+        }
+    
+    });
--- docs/assets/components/page-header.js
+++ docs/assets/components/page-header.js
@@ -1,25 +1,29 @@
-class PageHeader extends HTMLElement
-{
-    constructor() 
+customElements.define(
+    "page-header", 
+    class extends HTMLElement
     {
-        super();
+        constructor() 
+        {
+            super();
 
-    }
 
-    connectedCallback()
-    {
-        this.innerHTML =
-        `
-            <header style="margin: 0px;">
-                <div class="vertical-center" style="text-align: center; color: #FAFAFA;">
-                    <h1>Practical ARM Assembly</h1>
-                    <p>A collection of documents, notes and other material created to accompany the <a href="https://github.com/josh-reeves/practical_arm_assembly">Practical Arm Assembly</a> repository.</p>
-                </div>
-            </header>
-        `;
+        }
 
-    }
+        connectedCallback()
+        {
+            let content = this.innerHTML;
 
-}
+            this.innerHTML =
+            `
+                <header class="page-header">
+                    <div class="vertical-center">
+                        <h1>Practical ARM Assembly</h1>
+                        <p>A collection of documents, notes and other material created to accompany the <a href="https://github.com/josh-reeves/practical_arm_assembly">Practical Arm Assembly</a> repository.</p>
+                        ${content}    
+                    </div>
+                </header>
+            `;
 
-customElements.define("page-header", PageHeader);
+        }
+
+    });
