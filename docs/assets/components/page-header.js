90637079cc66ef517d8b67429e5f91116f139e79
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2024-12-19 14:40:07 -0600

    Additional changes to documentation styles.

--- docs/assets/components/page-header.js
+++ docs/assets/components/page-header.js
@@ -16,10 +16,11 @@ customElements.define(
             this.innerHTML =
             `
                 <header class="page-header">
-                    <div class="vertical-center">
+                    <img src="/practical_arm_assembly/assets/icons/icon_cpu_001_fill_003.svg" width="96" style="display: block"/>
+                    <div class="left">
                         <h1>Practical ARM Assembly</h1>
-                        <br>
-                        <p>A collection of documents, notes and other material created to accompany the <a href="https://github.com/josh-reeves/practical_arm_assembly">Practical Arm Assembly</a> repository.</p>
+                        <!--<br>
+                        <p>A collection of documents, notes and other material created to accompany the <a href="https://github.com/josh-reeves/practical_arm_assembly">Practical Arm Assembly</a> repository.</p>-->
                         ${content}   
                     </div>
                 </header>
