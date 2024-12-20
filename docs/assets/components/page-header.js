931d99c3525f5323f8a0378ebcccda7fe8f38853
Josh Reeves <jr.accounts@joshreeves.net>
2024-12-19 23:01:23 -0600

    Continued working on styles and introduction.

--- docs/assets/components/page-header.js
+++ docs/assets/components/page-header.js
@@ -16,8 +16,8 @@ customElements.define(
             this.innerHTML =
             `
                 <header class="page-header">
-                    <img src="/practical_arm_assembly/assets/icons/icon_cpu_001_fill_003.svg" width="96" style="display: block"/>
                     <div class="left">
+                        <img src="/practical_arm_assembly/assets/icons/icon_cpu_001_fill_003.svg" width=48/>
                         <h1>Practical ARM Assembly</h1>
                         <!--<br>
                         <p>A collection of documents, notes and other material created to accompany the <a href="https://github.com/josh-reeves/practical_arm_assembly">Practical Arm Assembly</a> repository.</p>-->
