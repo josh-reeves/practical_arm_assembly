42b1e98c8819bfa1cea75c22eb620d6ded3fa0fb
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2024-12-21 23:49:27 -0600

    Continued working on lesson 1 and fixed bug from previous commit.

--- docs/assets/components/page-header.js
+++ docs/assets/components/page-header.js
@@ -16,7 +16,7 @@ customElements.define(
             `
             <header class="page-header">
                 <div class="left">
-                <img src="\/assets/icons/icon_cpu_001_fill_003.svg" width=48/>
+                <img src="/practical_arm_assembly/assets/icons/icon_cpu_001_fill_003.svg" width=48/>
                     <h1>Practical ARM Assembly</h1>
                     <!--<br>
                     <p>A collection of documents, notes and other material created to accompany the <a href="https://github.com/josh-reeves/practical_arm_assembly">Practical Arm Assembly</a> repository.</p>-->
