a96e2df2d4b8c4995bb741138c4f5c220e100623
josh-reeves <jr.accounts@joshreeves.net>
2025-03-06 21:42:19 -0600

    reworks source code for branching examples.

--- source/section-08/a64/branching.s
+++ source/section-08/a64/branching.s
@@ -1,14 +1,14 @@
 .global _start
 
 _start:
-	mov r1, #0x4 // Moves the value 5 into r1.
+	mov x1, #0x4 // Moves the value 5 into r1.
 
-	cmp r1, #0x5 // Compares the value stored in r1 with the value 5 and updates the condition flags based on the result.
+	cmp x1, #0x5 // Compares the value stored in r1 with the value 5 and updates the condition flags based on the result.
 
 	bmi cond1 // The MI extension indicates that the instruction should only be executed if the N condition flag is set to 1.
 
 cond1:
-	mov r0, #0x1
+	mov x0, #0x1
 
 	/* Unless steps are taken to redirect the program's flow, it will continue
 	 * until the software interrupt instruction is executed. For this reason the
@@ -18,12 +18,12 @@ cond1:
 	b exit // Jump toexit label.
 
 cond2:
-	mov r0, #0x2
+	mov x0, #0x2
 
 	/* Note that the redirection from cond1 is not required at this point, as
 	 * execution will continue from here into the end label regardless.*/
 
 exit:
-	mov r7, #0x1
+	mov x8, #0x5d
 	svc 0
 
